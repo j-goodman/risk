a58ddca89678cc7b3d887a4a455fd7754263d2ae
John Goodman <johncurtisgoodman@gmail.com>
2016-09-10 16:28:51 -0400

    Build out landing page

--- board.js
+++ board.js
@@ -1,15 +1,16 @@
 var onLoad;
 
 onLoad = function () {
-  var slide; var headline;
+  var slide; var headline; var title;
   slide = document.getElementsByTagName('slide');
   headline = document.getElementsByClassName('headline')[0];
+  title = document.getElementsByClassName('main-banner')[0];
   headline.className += ' expant';
   window.setTimeout(function () {
-    console.log("!");
     slide[0].className = 'blue-marble-slide';
     slide[1].className = 'welcome-slide active';
-  }, 3400);
+    title.className += ' risen';
+  }, 2600);
 };
 
 window.onload = onLoad;
